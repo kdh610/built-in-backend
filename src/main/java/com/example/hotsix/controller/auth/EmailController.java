837b56c204c8ffcbdee64d6e9a95a0dcba598bd7
k_dh610 <k_dh610@naver.com>
2025-05-25 03:57:00 +0900

    refactor: email login refactoring

--- src/main/java/com/example/hotsix/controller/auth/EmailController.java
+++ src/main/java/com/example/hotsix/controller/auth/EmailController.java
@@ -52,47 +52,22 @@ public class EmailController {
     // 메일로 온 링크로 들어오면 token검증하고 유효하면 로그인 처리
     @GetMapping("/email-login")
     public void emailLogin(@RequestParam("code") String code, @CookieValue(value = "refresh", required = false) Cookie refresh, HttpServletResponse response) throws IOException {
-        String email = null;
         try{
-            if(jwtUtil.validateToken(code)){
-                email = jwtUtil.getEmail(code);
+            jwtUtil.validateToken(code);
+            String email = jwtUtil.getEmail(code);
+
+            if(email == null) {
+                Map<String, Cookie> cookies = loginService.login(email);
+
+                response.addCookie(cookies.get("access"));
+                response.addCookie(cookies.get("refresh"));
+                response.sendRedirect(clinetHost+"/afterlogin");
             }
         }catch (BuiltInException e){
-            log.info(e.getMessage());
-            try {
-                if(refresh!=null && redisTokenService.isTokenInRedis(jwtUtil.getId(refresh.getValue()).toString())){
-                    // 리프레시 토큰 삭제
-                    log.info("Redis refresh토큰 삭제");
-                    redisTokenService.deleteRefreshToken(jwtUtil.getId(refresh.getValue()).toString());
-
-                    //리프레시 토큰 쿠키 값 0
-                    Cookie cookie = new Cookie("refresh",null);
-                    cookie.setMaxAge(0);
-                    cookie.setPath("/");
-
-                    log.info("로그아웃 완료");
-                    response.addCookie(cookie);
-                    response.setStatus(HttpServletResponse.SC_OK);
-                }
-
-                response.sendRedirect(clinetHost);
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-
-        log.info("email {}", email);
-        if(email != null) {
-
-        Map<String, Cookie> cookies = loginService.login(email);
-
-        response.addCookie(cookies.get("access"));
-        response.addCookie(cookies.get("refresh"));
-        response.sendRedirect(clinetHost+"/afterlogin");
+            throw e;
         }
     }
 
-    //메일로 온 링크로 들어와서 토큰이 유요하면 프로필 작성 페이지로 리다이렉트
     @GetMapping("/register")
     public void register(@RequestParam("code") String code, HttpServletResponse response){
         try{
@@ -114,10 +89,8 @@ public class EmailController {
         }
     }
 
-    // 프로필 작성 후 가입 버튼 누를시 가입완료
     @PostMapping(value = "/signup", consumes = "application/json;charset=UTF-8")
     public MemberDto signup(@RequestBody MemberDto member){
-        log.info("member {}",member.toString());
         MemberDto memberDto = loginService.signUp(Member.fromDto(member));
         return memberDto;
     }
