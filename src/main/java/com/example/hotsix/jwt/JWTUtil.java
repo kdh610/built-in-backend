156f806d1a6c15ae25affe37ab41fc7b783ee878
k_dh610 <k_dh610@naver.com>
2025-05-25 19:35:24 +0900

    refactor: reissue controller

--- src/main/java/com/example/hotsix/jwt/JWTUtil.java
+++ src/main/java/com/example/hotsix/jwt/JWTUtil.java
@@ -81,11 +81,20 @@ public class JWTUtil {
         String category = getCategory(token);
 
         if(!category.equals("access")) {
-            log.info("카테고리가 access가 아니다");
             throw new BuiltInException(Process.INVALID_TOKEN);
         }
         return true;
     }
+
+    public Boolean isTokenTypeRefresh(String token){
+        String category = getCategory(token);
+
+        if(!category.equals("refresh")) {
+            throw new BuiltInException(Process.INVALID_TOKEN);
+        }
+        return true;
+    }
+
 
     public long getRemainingTime(String token) {
         Date expiration1 = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration();
